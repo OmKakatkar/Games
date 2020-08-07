3528761427fc0e0a3fab341252638ed702dcf08c
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2020-08-07 18:11:08 +0530

    Window and players set
    Adding the code for window, paddle a and paddle b. Added the ball. Defined borders and movement to ball. Added movement to Paddles with border control

--- Ping Pong/Ping_Pong.py
+++ Ping Pong/Ping_Pong.py
@@ -1,12 +1,79 @@
 import turtle
 
+#Create window and setup
 win = turtle.Screen()
 win.bgcolor("Black")
+win.setup(800,600)
+win.title("PING PONG")
 win.tracer(0)
 
+#Create paddle A
+paddle_a = turtle.Turtle()
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid = 4, stretch_len = 1)
+paddle_a.pu()
+paddle_a.goto(-370,0)
 
+#Create paddle B
+paddle_b = turtle.Turtle()
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid = 4, stretch_len = 1)
+paddle_b.pu()
+paddle_b.goto(360,0)
 
+#Create the ball
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("white")
+ball.pu()
+ball.dx = 0.5
+ball.dy = 0.5
+
+def borderCheck():
+    borderX = 380
+    borderY = 280
+    if ball.ycor() > borderY:
+        ball.sety(borderY)
+        ball.dy = -ball.dy
+        
+    if ball.ycor() < -borderY:
+        ball.sety(-borderY)
+        ball.dy = -ball.dy
+    
+    if ball.xcor() < -borderX:
+        ball.setx(-borderX)
+        ball.dx = -ball.dx
+
+    if ball.xcor() > borderX:
+        ball.setx(borderX)
+        ball.dx = -ball.dx
+
+def paddle_a_up():
+    if paddle_a.ycor() < 260:
+        paddle_a.sety(paddle_a.ycor() + 10)
+def paddle_a_dn():
+    if paddle_a.ycor() > -250:
+        paddle_a.sety(paddle_a.ycor() - 10)
+def paddle_b_up():
+    if paddle_b.ycor() < 260:
+        paddle_b.sety(paddle_b.ycor() + 10)
+def paddle_b_dn():
+    if paddle_b.ycor() > -250:
+        paddle_b.sety(paddle_b.ycor() - 10)
+
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_dn, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_dn, "Down")
 while True:
     win.update()
 
-win.bye()
+    ball.sety(ball.ycor() + ball.dy)
+    ball.setx(ball.xcor() + ball.dx)
+
+    borderCheck()
+    
+win.done()
