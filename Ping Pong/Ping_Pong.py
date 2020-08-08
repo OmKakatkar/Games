ed667531635b73ae7596d03c6e089d583616607f
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2020-08-08 19:28:09 +0530

    Update Ping Pong
    Added collision to paddles.
    Increased speed of all turtles.
    Added some comments.
    Ball speeds up while moving downwards.
    Added score.
    Score updation left.

--- Ping Pong/Ping_Pong.py
+++ Ping Pong/Ping_Pong.py
@@ -9,6 +9,7 @@ win.tracer(0)
 
 #Create paddle A
 paddle_a = turtle.Turtle()
+paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid = 4, stretch_len = 1)
@@ -17,6 +18,7 @@ paddle_a.goto(-370,0)
 
 #Create paddle B
 paddle_b = turtle.Turtle()
+paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid = 4, stretch_len = 1)
@@ -25,17 +27,28 @@ paddle_b.goto(360,0)
 
 #Create the ball
 ball = turtle.Turtle()
+ball.speed(0)
 ball.shape("circle")
 ball.color("white")
 ball.pu()
-ball.dx = 0.4
-ball.dy = 0.4
+ball.dx = 0.3
+ball.dy = 0.3
 
+#Create Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.hideturtle()
+pen.color("white")
+pen.pu()
+pen.goto(0,260)
+pen.write("Player A:0  Player B:0", align="center", font=("Courier", 24, "normal"))
+
+#Check borders for ball
 def borderCheck():
     borderX = 380
     borderY = 280
     if ball.ycor() > borderY + 10:
-        ball.sety(borderY)
+        ball.sety(borderY + 10)
         ball.dy = -ball.dy
         
     if ball.ycor() < -borderY:
@@ -43,42 +56,63 @@ def borderCheck():
         ball.dy = -ball.dy
     
     if ball.xcor() < -borderX - 10:
-        ball.setx(-borderX)
+        ball.goto(0,0)
         ball.dx = -ball.dx
+        pen.clear()
 
     if ball.xcor() > borderX:
-        ball.setx(borderX)
+        ball.goto(0,0)
         ball.dx = -ball.dx
 
+#Colliding ball with paddle_a
 def paddle_a_check():
-    if ball
+    if (ball.xcor() < -360 and ball.xcor() > -370 ) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-360)
+        ball.dx = -ball.dx
 
+#Colliding ball with paddle_b
+def paddle_b_check():
+    if (ball.xcor() > 350 and ball.xcor() < 360 ) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50): 
+        ball.setx(350)
+        ball.dx = -ball.dx
+        
+#Moving both paddles
 def paddle_a_up():
     if paddle_a.ycor() < 260:
         paddle_a.sety(paddle_a.ycor() + 10)
+
 def paddle_a_dn():
     if paddle_a.ycor() > -250:
         paddle_a.sety(paddle_a.ycor() - 10)
+
 def paddle_b_up():
     if paddle_b.ycor() < 260:
         paddle_b.sety(paddle_b.ycor() + 10)
+
 def paddle_b_dn():
     if paddle_b.ycor() > -250:
         paddle_b.sety(paddle_b.ycor() - 10)
 
+#Keyboard binding
 win.listen()
 win.onkeypress(paddle_a_up, "w")
 win.onkeypress(paddle_a_dn, "s")
 win.onkeypress(paddle_b_up, "Up")
 win.onkeypress(paddle_b_dn, "Down")
-while True:
-    win.update()
 
+while True:
+
+
+    #Move the ball
     ball.sety(ball.ycor() + ball.dy)
     ball.setx(ball.xcor() + ball.dx)
 
     borderCheck()
     paddle_a_check()
     paddle_b_check()
+
+    #Autopilot
+    #paddle_a.sety(ball.ycor())     
+    #paddle_b.sety(ball.ycor())
     
 win.done()
