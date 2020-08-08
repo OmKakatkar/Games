67d2dc376691af1dd9000d2ec8ed4f5ee332bce0
Om Kakatkar <68369994+OmKakatkar@users.noreply.github.com>
2020-08-08 20:43:31 +0530

    Updating Ping Pong
    Added score updation.
    To tweak moves of the paddles.

--- Ping Pong/Ping_Pong.py
+++ Ping Pong/Ping_Pong.py
@@ -43,14 +43,19 @@ pen.pu()
 pen.goto(0,260)
 pen.write("Player A:0  Player B:0", align="center", font=("Courier", 24, "normal"))
 
+score_a = 0
+score_b = 0
+
 #Check borders for ball
 def borderCheck():
     borderX = 380
     borderY = 280
+    global score_a,score_b
+    
     if ball.ycor() > borderY + 10:
         ball.sety(borderY + 10)
         ball.dy = -ball.dy
-        
+            
     if ball.ycor() < -borderY:
         ball.sety(-borderY)
         ball.dy = -ball.dy
@@ -58,12 +63,15 @@ def borderCheck():
     if ball.xcor() < -borderX - 10:
         ball.goto(0,0)
         ball.dx = -ball.dx
-        pen.clear()
-
+        score_b += 1
+        score_update()
+        
     if ball.xcor() > borderX:
         ball.goto(0,0)
         ball.dx = -ball.dx
-
+        score_a += 1
+        score_update()
+        
 #Colliding ball with paddle_a
 def paddle_a_check():
     if (ball.xcor() < -360 and ball.xcor() > -370 ) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
@@ -76,22 +84,31 @@ def paddle_b_check():
         ball.setx(350)
         ball.dx = -ball.dx
         
-#Moving both paddles
+#Moving paddle A Up
 def paddle_a_up():
     if paddle_a.ycor() < 260:
-        paddle_a.sety(paddle_a.ycor() + 10)
+        paddle_a.sety(paddle_a.ycor() + 20)
 
+#Moving paddle A Down
 def paddle_a_dn():
     if paddle_a.ycor() > -250:
-        paddle_a.sety(paddle_a.ycor() - 10)
+        paddle_a.sety(paddle_a.ycor() - 20)
 
+#Moving paddle B Up
 def paddle_b_up():
     if paddle_b.ycor() < 260:
-        paddle_b.sety(paddle_b.ycor() + 10)
+        paddle_b.sety(paddle_b.ycor() + 20)
 
+#Moving paddle B Down
 def paddle_b_dn():
     if paddle_b.ycor() > -250:
-        paddle_b.sety(paddle_b.ycor() - 10)
+        paddle_b.sety(paddle_b.ycor() - 20)
+
+#Updating Score
+def score_update():
+    global score_a,score_b
+    pen.clear()
+    pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
 
 #Keyboard binding
 win.listen()
@@ -102,11 +119,15 @@ win.onkeypress(paddle_b_dn, "Down")
 
 while True:
 
-
+    win.update()
+    
     #Move the ball
     ball.sety(ball.ycor() + ball.dy)
     ball.setx(ball.xcor() + ball.dx)
 
+    #paddle_a.sety(paddle_a.ycor() + 10)
+    #paddle_b.sety(paddle_a.ycor() + 10)
+    
     borderCheck()
     paddle_a_check()
     paddle_b_check()
